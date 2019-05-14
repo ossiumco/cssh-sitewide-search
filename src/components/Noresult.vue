d2958eb5d2cf52d29ac36b2797d138a948141de4
bojana <44167585+Lovely-Fellow@users.noreply.github.com>
2019-05-14 16:31:14 +0200

    Update

---
+++ src/components/Noresult.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <p class="result_title">
+    No Results Found from {{index.label}}
+  </p>
+  <hr class="result_title_hr">
+  <div class="content noresult" > 
+    <h4>
+      <small>No results were found for your search. Would you lkie to retry your search across all of CSSH?</small>
+      <br>
+    </h4>
+    <button>
+      Search All of CSSH
+    </button>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['index'],
+  data()
+  {
+    return {
+ 
+
+    }
+  }
+}
+
+</script>
+<style lang="scss">
+
+</style>
---
+++ src/components/Profiles.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="result-item row">
+  <div v-if="hit.images.thumbnail && hit.images.thumbnail.url" class="photo" >
+    <img :src="hit.images.thumbnail.url" alt class="profileImg">
+  </div>
+  <div v-if="!hit.images.thumbnail || !hit.images.thumbnail.url" class="photo" >
+    <img src="https://www.deedsalone.com/wp-content/uploads/2019/03/empty-face-athlete.svg" alt class="profileImg empty">
+  </div>                     
+  <div class="content">
+    <h4>
+      <ais-highlight attribute="post_title" :hit="hit"/>
+      <br>
+      <small>{{hit.content}}</small>
+      <br>
+    </h4>
+
+    <p v-html="hit.post_excerpt"></p>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+   props: ['hit'],
+  data()
+  {
+    return {
+
+
+    }
+  }
+}
+
+</script>
+<style lang="scss">
+
+</style>
